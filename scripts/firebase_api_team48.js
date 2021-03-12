05fb9b1d15a6869152e95d34b211277889356ebe
Brian Romaniuk <brirom@telus.net>
2021-03-12 13:07:10 -0800

    Add login.html and firbase js script file

---
+++ scripts/firebase_api_team48.js
@@ -0,0 +1,14 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBy4lYrAIkZSn5b4LphN9-fkMH3eHbDJHY",
+    authDomain: "team48-1800-project.firebaseapp.com",
+    projectId: "team48-1800-project",
+    storageBucket: "team48-1800-project.appspot.com",
+    messagingSenderId: "570719567738",
+    appId: "1:570719567738:web:b789ae381244a546a460c3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
