b663a9f5aaead543f27ed07699cd4fb4aa3ec85a
Subin_Moon <smoon44@my.bcit.ca>
2021-04-14 11:42:09 -0700

    Modify relative path

--- scripts/foodsData.js
+++ scripts/foodsData.js
@@ -4,7 +4,7 @@ read the data from category, and add item search bar
 */
 
 function readLocalJSON() {
-    fetch("./database/food.json")
+    fetch("../database/food.json")
         .then(function (response) {
             response.json()
                 .then(function (data) {
